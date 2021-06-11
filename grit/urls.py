2da6631ab336bdebcad1ad2d69cfd006c751f9f5
yasmeencole <yasmeencole1@mail.com>
2021-06-11 16:33:10 -0500

    completed auth.py, modified urls.py still working on it

--- grit/urls.py
+++ grit/urls.py
@@ -1,21 +1,19 @@
-"""grit URL Configuration
+# from django.urls import path
+# from django.conf.urls import include
+# from gritapi.views import register_user, login_user
+# from rest_framework import routers
+# from django.contrib import admin
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
+# # this parses the urls
+# # the register_user and login_user functions are imported into the module. Then they are used to map a route to that view.
+# router = routers.DefaultRouter(trailing_slash=False)
+# # /gametypes?label=blue
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', include(router.urls)),
+#     path('register', register_user),
+#     path('login', login_user),
+#     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
+# ]
