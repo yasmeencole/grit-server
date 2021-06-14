dc5795924b0f8f13275e50a379b87c2aa5a99877
yasmeencole <yasmeencole1@mail.com>
2021-06-14 11:06:57 -0500

    modified urls.py and created views/__init__.py

--- grit/urls.py
+++ grit/urls.py
@@ -1,19 +1,21 @@
-# from django.urls import path
-# from django.conf.urls import include
-# from gritapi.views import register_user, login_user
-# from rest_framework import routers
-# from django.contrib import admin
+from django.urls import path
+from django.conf.urls import include
+from gritapi.views import register_user, login_user
+from rest_framework import routers
+from django.contrib import admin
+# from gritapi.views import 
 
-# # this parses the urls
-# # the register_user and login_user functions are imported into the module. Then they are used to map a route to that view.
-# router = routers.DefaultRouter(trailing_slash=False)
-# # /gametypes?label=blue
+# this parses the urls
+# the register_user and login_user functions are imported into the module. Then they are used to map a route to that view.
+router = routers.DefaultRouter(trailing_slash=False)
+# /gametypes?label=blue
 
 
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('', include(router.urls)),
-#     path('register', register_user),
-#     path('login', login_user),
-#     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
-# ]
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('register', register_user),
+    path('login', login_user),
+    path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
+]
